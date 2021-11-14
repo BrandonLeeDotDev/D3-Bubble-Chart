79e55ebce100c81a3f3255abd6718775e7bd1ed1
BrandonLeeDotDev <72579318+BrandonLeeDotDev@users.noreply.github.com>
2021-11-14 01:36:33 -0600

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,13 +1,7 @@
 const nodes_to_create = 65;
 
-const margin = {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 50
-  },
-  width = 560 - margin.left - margin.right,
-  height = 460 - margin.top - margin.bottom;
+// set color to scale with width of chart
+// set X,Y buffers to scale with width of chart
 
 const axisXmaxLen = 1300;
 
@@ -16,6 +10,15 @@ const axisYminlen = 35;
 
 const radiusMax = 36;
 const radiusMin = 3;
+
+const margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 50
+  },
+  width = 550 - margin.left - margin.right,
+  height = 460 - margin.top - margin.bottom;
 
 const svg = d3.select("#chart")
   .append("svg")
@@ -26,7 +29,7 @@ const svg = d3.select("#chart")
 
 const x = d3.scaleLinear()
   .domain([0, axisXmaxLen])
-  .range([0, width - 20]);
+  .range([0, width]);
 
 svg.append("g")
   .attr("transform", `translate(0, ${height})`)
@@ -44,7 +47,7 @@ const z = d3.scaleLinear()
   .range([radiusMin, radiusMax]);
 
 const color = d3.scaleLinear()
-  .domain([0, 1, 3])
+  .domain([0, 2, 3])
   .range(["green", "orange", "red"]);
 
 const tooltip = d3.select("#tooltip")
@@ -116,8 +119,8 @@ function render() {
     .call(d3.drag().on("start", started));
 
   const simulation = d3.forceSimulation(nodes)
-    .force('x', d3.forceX(node => node.x_axis).strength(.2))
-    .force('y', d3.forceY(node => node.y_axis).strength(.2));
+    .force('x', d3.forceX(node => node.x_axis).strength(0.2))
+    .force('y', d3.forceY(node => node.y_axis).strength(0.2));
 
   function started(event) {
     const circle = d3.select(this).classed("dragging", true);
@@ -166,23 +169,29 @@ function render() {
   svg.append("text")
     .attr("class", "x_label")
     .attr("text-anchor", "end")
-    .attr("x", width - 20)
+    .attr("x", width)
     .attr("y", height - 6)
     .text("axisX Label (larger numebrs)")
     .attr("font-size", 13);
 
 }
 
+const xBuffer =  120;
+
+const yBuffer = 40;
+
+console.log(xBuffer)
+
 function randSize() {
   return Math.floor(Math.random() * (Math.floor(100) - Math.ceil(0)) + Math.ceil(0));
 }
 
 function randX() {
-  return Math.floor(Math.random() * (Math.floor(1000) - Math.ceil(130)) + Math.ceil(130));
+  return Math.floor(Math.random() * (Math.floor(axisXmaxLen - 200) - Math.ceil(xBuffer)) + Math.ceil(xBuffer));
 }
 
 function randY() {
-  return Math.floor(Math.random() * (Math.floor(77) - Math.ceil(47)) + Math.ceil(47));
+  return Math.floor(Math.random() * (Math.floor(axisYmaxLen - 5) - Math.ceil(radiusMax + 10)) + Math.ceil(radiusMax + 10));
 }
 
 let first_iteration_complete = false;
@@ -203,9 +212,6 @@ function build_modify_JSON() {
           radius: z(randSize())
         });
       } else {
-        const xBuffer = 120;
-
-        const yBuffer = 40;
 
         nodes.push({
           id: "node" + i,
